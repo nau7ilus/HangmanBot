f1bbf68c4d56151ca4d46015c7ced12c97367cb0
nau7ilus <filip@zelinskyi.de>
2024-08-20 19:44:09 +0200

    Remove application

--- application/schemas/authHeaders.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const S = require('fluent-json-schema');
-
-module.exports = S.object().prop('authorization', S.string().required());
--- application/schemas/error.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const S = require('fluent-json-schema');
-
-module.exports = S.object()
-  .prop('statusCode', S.number().required())
-  .prop('message', S.string().required())
-  .prop('error', S.string().required());
